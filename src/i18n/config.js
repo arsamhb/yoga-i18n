151aa43db25b4f01237b4f8986e122f031f5613a
Rsam <arsam.hb@gmail.com>
2023-11-11 15:43:30 +0330

    fa/default

--- src/i18n/config.js
+++ src/i18n/config.js
@@ -2,8 +2,8 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
 i18n.use(initReactI18next).init({
-  fallbackLng: 'en',
-  lng: 'en',
+  fallbackLng: 'fa',
+  lng: 'fa',
   resources: {
     en: {
       translations: require('./locales/en.translation.json')
@@ -16,6 +16,6 @@ i18n.use(initReactI18next).init({
   defaultNS: 'translations'
 });
 
-i18n.languages = ['en', 'fa'];
+i18n.languages = ['fa','en'];
 
 export default i18n;
